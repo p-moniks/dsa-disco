f4eafba4db6006af858832edf9c0f6e437f85fc4
p-moniks <curlii19@ymail.com>
2022-09-20 13:43:13 +0530

    roman numerals another func

--- Roman.java
+++ Roman.java
@@ -26,5 +26,19 @@ public class Roman {
 		
 		return rom;
     }
+
+  public String intToRoman(int n) {
+	    int[] A = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	    String[] B = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+	    StringBuilder sb = new StringBuilder();
+	    for(int i=0; i<A.length; i++) {
+	        while(n >= A[i]) {
+	            sb.append(B[i]);
+	            n -= A[i];
+	        }
+	    }
+	    return sb.toString();
+	}
+
 }
 
